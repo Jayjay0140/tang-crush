3ae250c5591fb5412b57c92589c3ef483aa74205
Jayjay0140 <86669931+Jayjay0140@users.noreply.github.com>
2021-10-17 13:14:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Dương Hạnh à!',
+    introDesc: `Em có muốn ngày nào đó mình ra khơi
+    Anh sẽ đem những nhớ thương mà đóng thuyền
+    Và tất cả những ngày mình xa xôi 
+    Nếu mà đem bán chắc là được cả đống tiền `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Mình sẽ ra khơi trên một cánh buồm đỏ thắm',
+    desc: 'Cuộc đời là đại dương hai đứa mình thì nhỏ lắm ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnNo: 'Không, Anh ơiii :3',
+    question:'Hôm nay anh mới nhớ,ngày mẹ sinh ra em,em đẹp như que kem và thật là xinh xắn,cuộc đời sao thật ngắn,chúc em sống thật vui,và luôn luôn yêu đời, cùng thật nhièu may  mắn,em luôn luôn khỏe khoắn, học giỏi và thành công',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Trái đất tròn lắm hy vọng ngày gần nhất em sẽ được gặp anh,e nhớ anh há há',
+    mess: 'Anh biết mà 🥰. Cảm ơn em nhiều 😘😘',
+    messDesc: 'Dù chỉ là bạn nhưng về Việt Nam thì cho anh mời em đi ăn hay đi chơi gì gì nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
